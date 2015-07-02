bd584a7fdc8ef70aa94d6e49d1678515781c01bf
taoqingfeng <165526381@qq.com>
2015-07-02 14:45:11 +0800

    treeList

--- treeList.js
+++ treeList.js
@@ -5,16 +5,16 @@ var server = http.createServer(function(req,res){
     res.writeHead(200,{"Content-type":"text/html;charset=utf-8"});//设置头部信息
     var url = req.url;//读取访问的url
     console.log(url);
-    var pro = process.cwd();
-    var msg = "";
+    var pro = process.cwd();//读取当前程序绝对路径
+    var msg = "";//输出字符串
     var urlParam;
     if(url == '/'){
         url = '';
     }
     urlParam = pro + url;
-    if(fs.existsSync(urlParam)){
-        fs.stat(urlParam,function(err,stats){
-            if(stats.isFile()){
+    if(fs.existsSync(urlParam)){//判断文件是否存在
+        fs.stat(urlParam,function(err,stats){//如果存在，则判断文件状态是文件夹还是文件
+            if(stats.isFile()){//如果是文件，则把文件输出出来
                 fs.readFile(urlParam,'utf-8',function(err,data){
                     if(err){
                         res.end('读取出错');
@@ -22,7 +22,7 @@ var server = http.createServer(function(req,res){
                         res.end(data + "<br><a href='/'>返回根目录</a>");
                     }
                 })
-            }else if(stats.isDirectory()){
+            }else if(stats.isDirectory()){//如果是文件夹，则读取文件夹目录
                 fs.readdir(urlParam,function(err,arr){
                     if(err){
                         res.end('读取文件出错');
